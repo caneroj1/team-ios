72bb352a617da9268b8b243668b885ef8c735222
caneroj1 <caneroj1@tcnj.edu>
2015-04-01 18:18:10 -0400

    gets profile picture from s3 now.

--- MusiciansWanted/EditProfileViewController.swift
+++ MusiciansWanted/EditProfileViewController.swift
@@ -75,14 +75,13 @@ class EditProfileViewController: UIViewController {
         }
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        
+//    }
+    
 
 }

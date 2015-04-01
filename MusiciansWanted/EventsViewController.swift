9f675510c5dfa73c6065957ceb754bb654a04fdf
Hank Harvey <harveyh1@tcnj.edu>
2015-03-31 21:11:41 -0400

    Working on error

---
+++ MusiciansWanted/EventsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventsViewController.swift
+//  MusiciansWanted
+//
+//  Created by hankharvey on 3/31/15.
+//  Copyright (c) 2015 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+    
+
+
+
+}

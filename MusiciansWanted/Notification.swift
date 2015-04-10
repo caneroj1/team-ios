23f697be1e097d68301efd23568a704341fe7569
caneroj1 <caneroj1@tcnj.edu>
2015-04-09 20:14:42 -0400

    updates to project. adding basic functionality for notifications in the news feed now.

---
+++ MusiciansWanted/Notification.swift
@@ -0,0 +1,23 @@
+//
+//  Notification.swift
+//  MW
+//
+//  Created by Joseph Canero on 4/9/15.
+//  Copyright (c) 2015 iOS Team. All rights reserved.
+//
+
+import Foundation
+
+class Notification {
+    let title: String
+    let location: String?
+    let distance: String?
+    let date: String
+    
+    init(title: String, date: String, location: String?, distance: String?) {
+        self.title = title
+        self.date = date
+        self.location = location
+        self.distance = distance
+    }
+}

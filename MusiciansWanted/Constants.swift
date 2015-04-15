7dfed5c58d0a726e1017f4dcf16c9c5025c89fd4
Hank Harvey <harveyh1@tcnj.edu>
2015-04-15 09:11:12 -0400

    Mobile analytics is working

---
+++ MusiciansWanted/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MW
+//
+//  Created by hankharvey on 4/15/15.
+//  Copyright (c) 2015 iOS Team. All rights reserved.
+//
+//  File will be added to .gitignore because it has credentials
+
+import Foundation
+
+//WARNING: To run this sample correctly, you must set the following constants.

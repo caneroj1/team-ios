c4f20a04f7828023a5aa19051bb9d351f5250547
Hank Harvey <harveyh1@tcnj.edu>
2015-05-06 00:37:00 -0400

    so you don't have to see verbose AWS

--- MusiciansWanted/AppDelegate.swift
+++ MusiciansWanted/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        AWSLogger.defaultLogger().logLevel = AWSLogLevel.Verbose
+        //AWSLogger.defaultLogger().logLevel = AWSLogLevel.Verbose
         
         
         let credentialsProvider = AWSCognitoCredentialsProvider(
